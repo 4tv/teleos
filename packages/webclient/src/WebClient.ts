9b82e06363a1c99feb70f06690b8e92c84978942
2g4 <snoqualmiemis@gmail.com>
2023-09-17 15:55:59 -0700

    feat: change getMode to static

--- packages/webclient/src/WebClient.ts
+++ packages/webclient/src/WebClient.ts
@@ -78,7 +78,7 @@ abstract class WebClient {
    * This method is typically used to adjust the behavior
    * of the client app depending on the mode in which it is running.
    */
-  public getMode(): ClientMode | undefined {
+  public static getMode(): ClientMode | undefined {
     const urlParams = new URLSearchParams(window.location.search);
 
     switch (urlParams.get('mode')) {
@@ -146,12 +146,12 @@ abstract class WebClient {
    * The loading animation will be shown on the host side
    * until this method is called.
    */
-  protected init(options?: ReadyOptions, mode = this.getMode()): void {
+  protected init(options?: ReadyOptions, mode = WebClient.getMode()): void {
     if (this.initialized) {
       throw new Error('App already initialized');
     }
 
-    if (this.getMode() !== mode) {
+    if (WebClient.getMode() !== mode) {
       throw new Error(`The class can only be used in ${mode} mode`);
     }
 
